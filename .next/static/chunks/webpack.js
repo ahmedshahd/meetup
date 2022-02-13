88bbb52a4bafe8552c49e8a97310d4f13cfe2ffb
Zalouma <ahmedshahd350@gmail.com>
2022-02-13 20:02:11 +0200

    adding MeetupDetail component and styling it

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6fea63536e2e59fbff4";
+/******/ 	var hotCurrentHash = "7cfffe97cd87294ae441";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://static.saltinourhair.com/wp-content/uploads/2019/03/23140923/cairo-pyramids-giza.jpg"
+      title="first meetup"
+      address="cairo, naser city 18 moaz eldoula"
+      description="the description"
+    />
+  );
+};
+export default MeetupDetails;
