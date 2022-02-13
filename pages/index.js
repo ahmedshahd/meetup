863f4bb62469576dae7416c25df3eb1274efc228
Zalouma <ahmedshahd350@gmail.com>
2022-02-13 19:21:54 +0200

    adding some dummy date to test components rendering

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "1",
+    title: "first meetup",
+    address: "cairo, naser city 18 moaz eldoula",
+    description: "that is our first meetup",
+    image:
+      "https://static.saltinourhair.com/wp-content/uploads/2019/03/23140923/cairo-pyramids-giza.jpg",
+  },
+  {
+    id: "2",
+    title: "seconed meetup",
+    address: "gize, tahrir city 18 elkhalifa",
+    description: "that is our seconed meetup",
+    image:
+      "https://cdn.theculturetrip.com/wp-content/uploads/2021/07/cairo.jpg",
+  },
+];
+const HomePage = () => {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
+};
+
+export default HomePage;
