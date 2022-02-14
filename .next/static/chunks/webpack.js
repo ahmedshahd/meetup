98802da9a09681089fb7c3940dfe71070529d3d2
Zalouma <ahmedshahd350@gmail.com>
2022-02-14 10:47:47 +0200

    adding dynamic routing by get static paths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e70451f7da09aba7d07a";
+/******/ 	var hotCurrentHash = "eca029172cbc85b6934d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,4 +9,37 @@ const MeetupDetails = () => {
     />
   );
 };
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "1",
+        },
+      },
+      {
+        params: {
+          meetupId: "2",
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  const { meetupId } = context.params;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupDate: {
+        image:
+          "https://static.saltinourhair.com/wp-content/uploads/2019/03/23140923/cairo-pyramids-giza.jpg",
+        title: "first meetup",
+        address: "cairo, naser city 18 moaz eldoula",
+        description: "the description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
